e116872f5ab0190f5e7c66e7a2bef9a2abfceef3
Oscar Lindqvist <olindqv@gmail.com>
2021-10-31 12:44:54 +0100

    create path exists function

--- validation/qValidator.js
+++ validation/qValidator.js
@@ -15,20 +15,33 @@ const isObject = (value) => {
   return typeof value === "object" && !Array.isArray(value) && value !== null;
 };
 
+const confirmPathExists = (object, pathArray) => {
+  if (pathArray.length === 1) {
+    return (
+      !isObject(object[pathArray[0]]) && object[pathArray[0]] !== undefined
+    );
+  }
+  if (!isObject(object[pathArray[0]])) {
+    return false;
+  }
+  return confirmPathExists(object[pathArray[0]], pathArray.slice(1));
+};
+
+console.log(
+  confirmPathExists(testObject, ["hey"])
+);
+
 const getObjectPaths = (object, basePath = "", pathsArray = []) => {
   const keys = Object.keys(object);
   keys.forEach((key) => {
     if (isObject(object[key])) {
-      getObjectPaths(
-        object[key],
-        `${basePath}${key}.`,
-        pathsArray
-      );
+      getObjectPaths(object[key], `${basePath}${key}.`, pathsArray);
     } else {
-      pathsArray.push(`${basePath}${key}`);
+      pathsArray.push({
+        path: `${basePath}${key}`.split("."),
+        value: object[key],
+      });
     }
   });
   return pathsArray;
 };
-
-console.log(getObjectPaths(testObject));
