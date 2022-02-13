8de4df5b7e185f862ce76f0ce9372ea22b6c6a3b
Oscar Lindqvist <olindqv@gmail.com>
2022-02-13 02:31:16 -0800

    create req res objects

---
+++ src/features/request/index.ts
@@ -0,0 +1,33 @@
+import http from "http";
+
+export class Qres {
+  httpRes: http.ServerResponse;
+  statusCode: number | null;
+
+  constructor(httpRes: http.ServerResponse) {
+    this.httpRes = httpRes;
+    this.statusCode = null;
+  }
+
+  status(statusCode: number) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  json(data: object) {
+    this.httpRes.writeHead(this.statusCode || 200, { "Content-Type": "application/json" });
+    this.httpRes.end(JSON.stringify(data));
+  }
+}
+
+export class Qreq {
+  body: object;
+  query: object;
+  params: object;
+
+  constructor(body: object, query?: object | null, params?: object | null) {
+    this.body = body;
+    this.query = query || {};
+    this.params = params || {};
+  }
+}
