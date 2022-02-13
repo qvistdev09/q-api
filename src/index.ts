de40ed965a3f982839c549e74c36c1db2ccc025a
Oscar Lindqvist <olindqv@gmail.com>
2022-02-13 00:45:10 -0800

    typescript tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-const hey = () => {
-  console.log("Hey");
+interface Human {
+  name: string;
+  age: number;
+}
+
+export const hey = (human: Human) => {
+  console.log(human.name);
+  console.log(human.age);
+};
+
+export const makeHuman = (name: string, age: number): Human => {
+  return {
+    name,
+    age,
+  };
 };
