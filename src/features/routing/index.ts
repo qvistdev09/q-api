daa22d121c4be745e89c591f01c61480f1a542d7
Oscar Lindqvist <olindqv@gmail.com>
2022-02-13 05:08:32 -0800

    move methods into routing

--- src/features/routing/index.ts
+++ src/features/routing/index.ts
@@ -1,5 +1,36 @@
 import FS from "fs";
-import { UrlMatcherResult } from "../../types";
+import { MiddleWare, UrlMatcherResult } from "../../types";
+import { Qreq, Qres } from "../request";
+
+const runNextMiddleware = (
+  chain: Array<MiddleWare>,
+  index: number,
+  req: Qreq,
+  res: Qres,
+  errorHandler: MiddleWare
+) => {
+  const scheduledMiddleware = chain[index];
+  index++;
+  if (scheduledMiddleware) {
+    scheduledMiddleware(req, res, (error) => {
+      if (error) {
+        errorHandler(req, res);
+        return;
+      }
+      runNextMiddleware(chain, index, req, res, errorHandler);
+    });
+  }
+};
+
+export const createRouteHandler = (
+  chain: Array<MiddleWare>,
+  errorHandler: MiddleWare
+): MiddleWare => {
+  return (req: Qreq, res: Qres) => {
+    let index = 0;
+    runNextMiddleware(chain, index, req, res, errorHandler);
+  };
+};
 
 const indexRecursively = (basePath: string, pathsArray: Array<string>) => {
   const directoryContents = FS.readdirSync(basePath);
