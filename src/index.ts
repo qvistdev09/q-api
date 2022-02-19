4c111dcde47e045708ca8cb77f9a4d5176c68a78
Oscar Lindqvist <olindqv@gmail.com>
2022-02-19 05:14:32 -0800

    create middleware fn

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Api } from "./features/api";
-import { MiddleWare } from "./types";
+import { Middleware } from "./features/middleware";
+import { MiddlewareFunction } from "./features/middleware/types";
 
 const Q = {
   createApi: (basePath: string, errorHandler: MiddleWare) => new Api(basePath, errorHandler),
+  createMiddleware: (middlewareFunction: MiddlewareFunction) => new Middleware(middlewareFunction),
 };
 
 export default Q;
