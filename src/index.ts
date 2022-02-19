533ddfdec8b264c8b1ea34c4eed7638716174d12
Oscar Lindqvist <olindqv@gmail.com>
2022-02-19 06:48:48 -0800

    create route creator fn

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Api } from "./base";
 import { Middleware } from "./features/middleware";
 import { MiddlewareFunction } from "./features/middleware/types";
+import { Route } from "./features/routing";
 
 const Q = {
   createApi: (basePath: string) => new Api(basePath),
+  createRoute: (middlewares: Middleware[]) => new Route(middlewares),
   createMiddleware: (middlewareFunction: MiddlewareFunction) => new Middleware(middlewareFunction),
 };
 
