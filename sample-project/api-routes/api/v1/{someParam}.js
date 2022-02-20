8b5103e8060f62bf2e639c9510dc99026248a17e
Oscar Lindqvist <olindqv@gmail.com>
2022-02-20 07:29:50 -0800

    expand on sample project

--- sample-project/api-routes/api/v1/{someParam}.js
+++ sample-project/api-routes/api/v1/{someParam}.js
@@ -10,3 +10,22 @@ module.exports.GET = Q.createRoute({
   ],
   useAuth: true,
 });
+
+module.exports.POST = Q.createRoute({
+  middlewares: [
+    Q.createMiddleware((req, res) => {
+      res.statusCode = 201;
+      res.responseData = { message: "resource created" };
+    }),
+  ],
+  useAuth: true,
+  reqBodySchema: Q.createSchema({
+    schema: {
+      data: {
+        name: Q.string().minLength(5),
+        age: Q.number().isInteger(),
+      },
+    },
+    requireAllProperties: true,
+  }),
+});
