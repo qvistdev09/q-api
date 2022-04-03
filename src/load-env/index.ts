ae0dc742d0bd63494b37e938805b271af6738e47
Oscar Lindqvist <olindqv@gmail.com>
2022-04-03 11:24:53 -0700

    update loadenv

--- src/load-env/index.ts
+++ src/load-env/index.ts
@@ -1,7 +1,7 @@
 import FS from "fs";
 
-export const loadEnv = () => {
-  const envData = FS.readFileSync("./.env", "utf-8");
+export const loadEnv = (dotEnvPath: string) => {
+  const envData = FS.readFileSync(dotEnvPath, "utf-8");
   envData.split("\n").forEach((envDataLine) => {
     const [key, value] = envDataLine.split("=");
     if (key && value) {
