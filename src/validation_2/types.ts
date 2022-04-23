999f1475a53048d633b9f8359209d0303e733040
Oscar Lindqvist <olindqv@gmail.com>
2022-04-23 02:55:18 -0700

    Update validation types

--- src/validation_2/types.ts
+++ src/validation_2/types.ts
@@ -1,3 +1,5 @@
+import { Base } from "./base";
+
 export interface ValidationError {
   path: string;
   error: string;
@@ -5,10 +7,14 @@ export interface ValidationError {
 
 export type DataSource = "body" | "header" | "path" | "query";
 
-export type ValidatorFunction<T> = (
+export type ValidatorFunction = (
   path: string,
   value: any,
   errors: ValidationError[],
   dataSource: DataSource,
-  setTransformedValue: (transformedValue: T) => void
+  setTransformedValue: (transformedValue: any) => void
 ) => void;
+
+export type SchemaDerivedInterface<T> = {
+  [P in keyof T]: T[P] extends Base<infer TS> ? TS : never;
+};
