a5eaca168c08cf312a7626cd63e8fc57d92a7649
Oscar Lindqvist <olindqv@gmail.com>
2022-05-01 05:12:54 -0700

    Update context class

--- src/context.ts
+++ src/context.ts
@@ -1,23 +1,18 @@
 import { IncomingHttpHeaders, IncomingMessage, ServerResponse } from "http";
 import { DecodedUser } from "./auth/types";
-import { JSON } from "./types";
 import { SchemaDerivedInterface } from "./validation/types";
 
 export class Context<BodySchema, PathSchema, QuerySchema> {
   httpRes: ServerResponse;
   httpReq: IncomingMessage;
   body: SchemaDerivedInterface<BodySchema>;
-  query: SchemaDerivedInterface<QuerySchema>;
   params: SchemaDerivedInterface<PathSchema>;
+  query: SchemaDerivedInterface<QuerySchema>;
   headers: IncomingHttpHeaders;
-  responseData: JSON;
-  statusCode: number | null;
 
   constructor(httpReq: IncomingMessage, httpRes: ServerResponse) {
     this.httpReq = httpReq;
     this.httpRes = httpRes;
-    this.responseData = {};
-    this.statusCode = null;
     this.headers = httpReq.headers;
     this.body = {} as SchemaDerivedInterface<BodySchema>;
     this.params = {} as SchemaDerivedInterface<PathSchema>;
