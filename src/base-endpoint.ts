2adf952ab437faf5e68be77506df81b7423c70ca
Oscar Lindqvist <olindqv@gmail.com>
2022-05-01 06:59:30 -0700

    Add services property to endpoint

--- src/base-endpoint.ts
+++ src/base-endpoint.ts
@@ -1,6 +1,7 @@
 import { HttpMethodHandler } from "./http-method-handler";
 
 export class BaseEndpoint {
+  public static services: string[];
   createHandler() {
     return new HttpMethodHandler();
   }
